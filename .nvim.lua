14aad490f77a70a53f36d83f96839dac31c2dbcb
Skylar Abruzese <skylar@abruzese.com>
2026-06-16 14:28:09 -0400

    make dune build have short display

--- .nvim.lua
+++ .nvim.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function(args)
     local root = vim.fs.root(args.buf, "Cargo.toml")
     if not root then return end
-    local cmd = ("cd %s && cargo refinedrust && cd output && dune build")
+    local cmd = ("cd %s && cargo refinedrust && cd output && dune build --display short")
       :format(vim.fn.shellescape(root))
     vim.bo[args.buf].makeprg = vim.fn.escape(cmd, "%#")
   end,
